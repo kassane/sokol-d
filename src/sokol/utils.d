82f862fa5f058f55a17268ebe34c731f15b943ad
Andre Weissflog <floooh@gmail.com>
2024-05-12 16:10:01 +0200

    remove the sgutil.asRange helper function

--- src/sokol/utils.d
+++ src/sokol/utils.d
@@ -7,21 +7,3 @@ string cStrTod(inout(char)* c_str) nothrow {
     for (; *end; end++){}
     return cast(string) c_str[0 .. end - start];
 }
-
-import sokol.gfx:Range;
-
-// WIP: helper function to convert "anything" to a Range struct
-Range asRange(T)(T val) @trusted {
-    import std.traits;
-    static if (isPointer!T) {
-       return Range(val, T.sizeof);
-    } else static if (is(T == float[]) || is(T == double[])) {
-       auto arr = val.dup;
-       return Range(&arr[0], arr.length * arr[0].sizeof);
-    } else static if (is(T == struct)) {
-       Range r = {ptr: cast(const(void)*)&val, size: T.sizeof};
-       return r;
-    } else {
-       static assert(0, "Cannot convert to range");
-    }
-}
