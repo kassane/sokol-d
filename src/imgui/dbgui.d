665d5f05d120cfb96ca34414e9e94782197e1b87
Matheus Catarino <matheus-catarino@hotmail.com>
2025-08-24 14:42:27 -0300

    examples: droptest fixes

--- src/imgui/dbgui.d
+++ src/imgui/dbgui.d
@@ -1,58 +1,62 @@
 module imgui.dbgui;
-import cimgui = imgui.cimgui;
-import simgui = sokol.imgui;
-import sgimgui = sokol.gfximgui;
-import log = sokol.log;
-import sapp = sokol.app;
+
+debug
+{
+    import cimgui = imgui.cimgui;
+    import simgui = sokol.imgui;
+    import sgimgui = sokol.gfximgui;
+    import log = sokol.log;
+    import sapp = sokol.app;
 
 extern (C):
 
-static sgimgui.Sgimgui sgctx;
+    static sgimgui.Sgimgui sgctx;
 
-void __dbgui_setup(int sample_count) @safe @nogc nothrow
-{
-    // setup debug inspection
-    sgimgui.Desc desc = {};
-    sgimgui.init(sgctx, desc);
-
-    //dfmt off
-    // setup the sokol-imgui utility
-    simgui.Desc simgui_desc = {
-        sample_count: sample_count,
-        logger: {func: &log.slog_func},
-    };
-    //dfmt on
-    simgui.setup(simgui_desc);
-}
-
-void __dbgui_shutdown() @safe @nogc nothrow
-{
-    sgimgui.discard(sgctx);
-    simgui.shutdown;
-}
-
-void __dbgui_draw() @safe @nogc nothrow
-{
-    //dfmt off
-    simgui.FrameDesc sframe = {
-        width: sapp.width,
-        height: sapp.height,
-        delta_time: sapp.frameDuration,
-        dpi_scale: sapp.dpiScale,
-    };
-    //dfmt on
-
-    simgui.newFrame(sframe);
-    if (cimgui.BeginMainMenuBar)
+    void __dbgui_setup(int sample_count) @safe @nogc nothrow
     {
-        sgimgui.drawMenu(sgctx, "sokol-gfx");
-        cimgui.EndMainMenuBar;
-    }
-    sgimgui.draw(sgctx);
-    simgui.render;
-}
+        // setup debug inspection
+        sgimgui.Desc desc = {};
+        sgimgui.init(sgctx, desc);
 
-void __dbgui_event(sapp.Event e) @safe @nogc nothrow
-{
-    simgui.handleEvent(e);
+        //dfmt off
+        // setup the sokol-imgui utility
+        simgui.Desc simgui_desc = {
+            sample_count: sample_count,
+            logger: {func: &log.slog_func},
+        };
+        //dfmt on
+        simgui.setup(simgui_desc);
+    }
+
+    void __dbgui_shutdown() @safe @nogc nothrow
+    {
+        sgimgui.discard(sgctx);
+        simgui.shutdown;
+    }
+
+    void __dbgui_draw() @safe @nogc nothrow
+    {
+        //dfmt off
+        simgui.FrameDesc sframe = {
+            width: sapp.width,
+            height: sapp.height,
+            delta_time: sapp.frameDuration,
+            dpi_scale: sapp.dpiScale,
+        };
+        //dfmt on
+
+        simgui.newFrame(sframe);
+        if (cimgui.BeginMainMenuBar)
+        {
+            sgimgui.drawMenu(sgctx, "sokol-gfx");
+            cimgui.EndMainMenuBar;
+        }
+        sgimgui.draw(sgctx);
+        simgui.render;
+    }
+
+    void __dbgui_event(const(sapp.Event)* ev) @trusted @nogc nothrow
+    {
+        simgui.simgui_handle_event(ev);
+    }
 }
